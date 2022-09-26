107af35f09620500484133c58b7237b3999e8c0e
Laidir88 <ktbriana@gmail.com>
2022-09-26 16:04:43 -0500

    ES6 not working

---
+++ node-example/app.js
@@ -0,0 +1,24 @@
+import { rectangle } from "./rectangle";
+
+function solveRect(l, w) {
+  console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+
+  rectangle(l, w, (err, rect) => {
+    if (err) {
+      console.log("ERROR:", err.message);
+    } else {
+      console.log(
+        `Area of rectangle with dimensions ${l}, ${w} is: ${rect.area()}`
+      );
+      console.log(
+        `Perimeter of rectangle with dimensions ${l}, ${w} is: ${rect.perimeter()}`
+      );
+    }
+  });
+  console.log("This statement is logged after the call to rect()");
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(5, -3);
